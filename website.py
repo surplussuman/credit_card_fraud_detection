76ebc1c37846ddcfb7056e98f054492704c2d864
Suman <92079502+surplussuman@users.noreply.github.com>
2024-03-31 13:54:10 +0530

    Add files via upload

--- website.py
+++ website.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+import streamlit as st
 
 
 dataset= pd.read_csv("creditcard.csv")
@@ -20,8 +21,8 @@ X_train, X_test, Y_train, Y_test = train_test_split(x,y,test_size=0.2, stratify=
 model = LogisticRegression()
 model.fit(X_train,Y_train)
 
-train_acc = accuracy_score(model.predict(X_train), y_train)
-test_acc = accuracy_score(model.predict(X_test),y_test)
+train_acc = accuracy_score(model.predict(X_train), Y_train)
+test_acc = accuracy_score(model.predict(X_test),Y_test)
 
 st.title("Credit Card fraud Detection")
 st.write("Enter the following features to check if the transaction if legitimate or fradulent")
@@ -32,10 +33,10 @@ input_df_lst= input_df.split(',')
 submit = st.button("Submit")
 
 if submit:
-    features = np.array[input_df_lst,dtype = np.float64]
+    features = np.array(input_df_lst, dtype=np.float64)
     prediction = model.predict(features.reshape(1,-1))
 
-    if predicions[0] == 0:
+    if prediction[0] == 0:
         st.write("Legitimate Transaction")
     else:
         st.write("Fraudulent Transaction")
