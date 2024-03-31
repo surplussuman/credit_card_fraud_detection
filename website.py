bb6b6c4be6a7d14904be8db6c7fec06559e9ebf2
Suman <92079502+surplussuman@users.noreply.github.com>
2024-03-31 10:45:32 +0530

    Add files via upload

---
+++ website.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+
+dataset= pd.read_csv("creditcard.csv")
+
+correct = dataset[dataset.Class==0]
+fraud = dataset[dataset.Class==1]
+
+sucess_sample = correct.sample(n=492)
+dataset2 = pd.concat([sucess_sample,fraud],axis=0)
+
+x = dataset2.drop('Class',axis=1)
+y = dataset2['Class']
+X_train, X_test, Y_train, Y_test = train_test_split(x,y,test_size=0.2, stratify=y,random_state=2)
+
+model = LogisticRegression()
+model.fit(X_train,Y_train)
+
+train_acc = accuracy_score(model.predict(X_train), y_train)
+test_acc = accuracy_score(model.predict(X_test),y_test)
+
+st.title("Credit Card fraud Detection")
+st.write("Enter the following features to check if the transaction if legitimate or fradulent")
+
+input_df = st.text_input("Enter all features")
+input_df_lst= input_df.split(',')
+
+submit = st.button("Submit")
+
+if submit:
+    features = np.array[input_df_lst,dtype = np.float64]
+    prediction = model.predict(features.reshape(1,-1))
+
+    if predicions[0] == 0:
+        st.write("Legitimate Transaction")
+    else:
+        st.write("Fraudulent Transaction")
